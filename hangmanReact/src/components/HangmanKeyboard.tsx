4787a0daed5c8e48fc763fdf41d00adba49f41b2
Alexander Nguyen <alex@Alexanders-MacBook-Air.local>
2023-09-13 17:15:08 -0700

    Update HangmanKeyboard: add letters and keys

--- hangmanReact/src/components/HangmanKeyboard.tsx
+++ hangmanReact/src/components/HangmanKeyboard.tsx
@@ -1,6 +1,18 @@
+const KEYS = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
 const HangmanKeyboard = () => {
   return (
-      <div></div>
+      <div style={{
+        display: 'grid',
+        gridTemplateColumns: 'repeat (auto-fit, minmax(75px, 1fr)',
+        gap: '.5rem'
+      }}> 
+        {KEYS.map(key => {
+          return (
+            <button key={key}></button>
+          )
+        })}
+      </div>
   )
 }
 
